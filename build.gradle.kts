8fb4b1ab0b239921dced3571a49f9c4d9d20b8f7
Maciej Malecki <malecki.maciej@gmail.com>
2020-10-21 22:48:25 +0200

    versions externalized

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,12 +2,16 @@ plugins {
     kotlin("jvm") version "1.4.10"
 }
 
-java.sourceCompatibility = JavaVersion.VERSION_14
-java.targetCompatibility = JavaVersion.VERSION_14
+java {
+    sourceCompatibility = JavaVersion.VERSION_14
+    targetCompatibility = JavaVersion.VERSION_14
+}
 
 allprojects {
     group = "gh.mm"
     version = "1.0.0-SNAPSHOT"
+
+
 
     tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
         kotlinOptions {
@@ -21,5 +25,6 @@ allprojects {
 
     repositories {
         mavenCentral()
+        jcenter()
     }
 }
--- domain/itemclasses/src/main/kotlin/mm/inventory/domain/itemclasses/ItemClasses.kt
+++ domain/itemclasses/src/main/kotlin/mm/inventory/domain/itemclasses/ItemClasses.kt
@@ -1,5 +1,22 @@
 package mm.inventory.domain.itemclasses
 
-data class Unit(val code: String, val name: String)
+import kotlinx.collections.immutable.ImmutableSet
 
-data class ItemAttribute(val name: String, val unit: Unit)
+data class ItemClass(
+        val name: String,
+        val description: String)
+
+data class Unit(
+        val code: String,
+        val name: String)
+
+data class ScalarType(
+        val unit: Unit)
+
+data class Attribute(
+        val name: String,
+        val unit: Unit)
+
+data class DictionaryValue(val value: String)
+
+data class DictionaryType(val values: ImmutableSet<DictionaryValue>)
