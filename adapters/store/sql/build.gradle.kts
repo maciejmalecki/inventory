1c34c921ea84b5fd8c884be5d94fefb2a3450898
Maciej Malecki <malecki.maciej@gmail.com>
2020-10-22 16:21:58 +0200

    liquibase

---
+++ adapters/store/sql/build.gradle.kts
@@ -0,0 +1,13 @@
+plugins {
+    kotlin("jvm")
+}
+
+val collectionsImmutableVersion: String by project
+
+dependencies {
+    implementation(project(":domain:itemclasses"))
+    implementation("org.liquibase.gradle:2.0.4")
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+    implementation("org.jetbrains.kotlinx:kotlinx-collections-immutable:$collectionsImmutableVersion")
+}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,3 +2,5 @@ rootProject.name = "inventory"
 
 include(":domain:itemclasses")
 include(":domain:items")
+
+include(":adapters:store:sql")
