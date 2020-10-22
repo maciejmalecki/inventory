387988d2dfd1564c0f5b3db8525be1708a3a230f
Maciej Malecki <malecki.maciej@gmail.com>
2020-10-22 15:27:32 +0200

    use cases

--- domain/itemclasses/src/main/kotlin/mm/inventory/domain/itemclasses/ItemClassRepository.kt
+++ domain/itemclasses/src/main/kotlin/mm/inventory/domain/itemclasses/ItemClassRepository.kt
@@ -2,4 +2,5 @@ package mm.inventory.domain.itemclasses
 
 interface ItemClassRepository {
     suspend fun findByName(name: String): ItemClass
+    suspend fun findByName(name: String, version: Int): ItemClassVersion
 }
