27307fc13c4eef2e7094e7159205f75d2d2d0025
Maciej Malecki <malecki.maciej@gmail.com>
2020-10-23 23:37:25 +0200

    simplified

--- adapters/store/sql/src/main/kotlin/mm/inventory/adapters/store/sql/UnitOfMeasurementSqlRepository.kt
+++ adapters/store/sql/src/main/kotlin/mm/inventory/adapters/store/sql/UnitOfMeasurementSqlRepository.kt
@@ -1,9 +1,6 @@
 package mm.inventory.adapters.store.sql
 
 import io.r2dbc.client.R2dbc
-import io.r2dbc.spi.Result
-import io.r2dbc.spi.Row
-import io.r2dbc.spi.RowMetadata
 import mm.inventory.domain.itemclasses.UnitOfMeasurement
 import mm.inventory.domain.itemclasses.UnitOfMeasurementRepository
 import reactor.core.publisher.Flux
@@ -12,8 +9,8 @@ class UnitOfMeasurementSqlRepository(private val r: R2dbc) : UnitOfMeasurementRe
 
     override suspend fun findAll(): Flux<UnitOfMeasurement> =
             r.withHandle {
-                it.select("select code, name from Units").mapResult { result ->
-                    result.map { row, _ ->
+                it.select("select code, name from Units").mapResult {
+                    it.map { row, _ ->
                         UnitOfMeasurement(row.get("code", String::class.java)!!, row.get("name", String::class.java)!!)
                     }
                 }
