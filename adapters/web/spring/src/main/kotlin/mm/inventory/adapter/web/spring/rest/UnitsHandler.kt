3a96d9ed37924703fbbfae0d51cb02399da8c3f3
Maciej Malecki <malecki.maciej@gmail.com>
2020-10-24 12:01:54 +0200

    unused imports

--- adapters/web/spring/src/main/kotlin/mm/inventory/adapter/web/spring/rest/UnitsHandler.kt
+++ adapters/web/spring/src/main/kotlin/mm/inventory/adapter/web/spring/rest/UnitsHandler.kt
@@ -1,7 +1,5 @@
 package mm.inventory.adapter.web.spring.rest
 
-import kotlinx.coroutines.reactive.awaitFirst
-import mm.inventory.domain.itemclasses.UnitOfMeasurement
 import mm.inventory.domain.itemclasses.UnitOfMeasurementRepository
 import org.springframework.http.MediaType
 import org.springframework.web.bind.annotation.RestController
