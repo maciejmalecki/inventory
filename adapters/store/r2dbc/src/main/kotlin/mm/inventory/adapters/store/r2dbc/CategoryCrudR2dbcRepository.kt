90ce1d259e1bd3d752edd67707896344ac40cc1e
Maciej Malecki <malecki.maciej@gmail.com>
2020-10-25 11:51:11 +0100

    Fix flux completion

--- adapters/store/r2dbc/src/main/kotlin/mm/inventory/adapters/store/r2dbc/CategoryCrudR2dbcRepository.kt
+++ adapters/store/r2dbc/src/main/kotlin/mm/inventory/adapters/store/r2dbc/CategoryCrudR2dbcRepository.kt
@@ -8,24 +8,24 @@ import kotlinx.collections.immutable.ImmutableSet
 import kotlinx.collections.immutable.toImmutableList
 import kotlinx.collections.immutable.toImmutableSet
 import kotlinx.coroutines.reactive.awaitFirst
+import kotlinx.coroutines.reactive.awaitSingle
 import mm.inventory.app.categories.Category
 import mm.inventory.app.categories.CategoryCrudRepository
 import org.reactivestreams.Publisher
 import reactor.core.publisher.Flux
 
-class CategoryCrudR2dbcRepository(private val r: R2dbc) : CategoryCrudRepository {
+class CategoryCrudR2dbcRepository(private val db: R2dbc) : CategoryCrudRepository {
 
     override suspend fun create(code: String, name: String): Category =
-            r.inTransaction {
+            db.inTransaction {
                 insertIntoCategories(it, code, name).flatMap { categoryId ->
                     insertRootIntoCategoryTreePath(it, categoryId)
                             .thenMany(selectCategoryById(it, categoryId))
                 }
-            }.awaitFirst()
-
+            }.awaitSingle()
 
     override suspend fun create(code: String, name: String, parentId: Long): Category =
-            r.inTransaction { handle ->
+            db.inTransaction { handle ->
                 insertIntoCategories(handle, code, name).flatMap { categoryId ->
                     insertRootIntoCategoryTreePath(handle, categoryId).flatMap {
                         insertIntoCategoryTreePath(handle, parentId, categoryId).flatMap {
@@ -33,24 +33,24 @@ class CategoryCrudR2dbcRepository(private val r: R2dbc) : CategoryCrudRepository
                         }
                     }
                 }
-            }.awaitFirst()
+            }.awaitSingle()
 
     override suspend fun findPathById(id: Long): ImmutableList<Category> =
-            r.withHandle {
+            db.withHandle {
                 selectPath(it, id)
             }.collectList().map {
                 it.toImmutableList()
-            }.awaitFirst()
+            }.awaitSingle()
 
     override suspend fun findAllRoot(): ImmutableSet<Category> =
-            r.withHandle {
+            db.withHandle {
                 selectCategoryWithNoParent(it)
             }.collectList().map {
                 it.toImmutableSet()
             }.awaitFirst()
 
     override suspend fun findAll(parentId: Long): ImmutableSet<Category> =
-            r.withHandle {
+            db.withHandle {
                 selectCategoryForParent(it, parentId)
             }.collectList().map {
                 it.toImmutableSet()
