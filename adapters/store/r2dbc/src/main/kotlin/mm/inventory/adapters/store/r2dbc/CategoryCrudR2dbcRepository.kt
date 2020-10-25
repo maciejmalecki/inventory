de92b3bc78d7b50418beebace1de34871646cb6b
Maciej Malecki <malecki.maciej@gmail.com>
2020-10-25 22:42:49 +0100

    Simplify client usage

--- adapters/store/r2dbc/src/main/kotlin/mm/inventory/adapters/store/r2dbc/CategoryCrudR2dbcRepository.kt
+++ adapters/store/r2dbc/src/main/kotlin/mm/inventory/adapters/store/r2dbc/CategoryCrudR2dbcRepository.kt
@@ -55,7 +55,9 @@ class CategoryCrudR2dbcRepository(private val db: R2dbc) : CategoryCrudRepositor
 
     private fun insertIntoCategories(it: Handle, code: String, name: String): Flux<Long> =
             it.execute("INSERT INTO Categories (code, name) VALUES ($1, $2)", code, name).flatMap { _ ->
-                it.select("SELECT LASTVAL()").mapResult(extractLong(0))
+                it.select("SELECT LASTVAL()").mapRow { row ->
+                    row.get(0) as Long
+                }
             }
 
     private fun insertRootIntoCategoryTreePath(it: Handle, categoryId: Long): Flux<Int> =
@@ -68,35 +70,24 @@ class CategoryCrudR2dbcRepository(private val db: R2dbc) : CategoryCrudRepositor
                             |FROM Categories_Tree_Path p, Categories_Tree_Path c
                             |WHERE p.descendant_id=$1 AND c.ancestor_id=$2""".trimMargin(), parentId, leafId)
 
-    private fun extractLong(param: Int): (Result) -> Publisher<Long> = { result: Result ->
-        result.map { row, _ -> row.get(param) as Long }
-    }
-
     private fun selectCategoryById(it: Handle, id: Long): Flux<Category> =
-            it.select("SELECT category_id, code, name FROM Categories WHERE category_id=$1", id).mapResult { result ->
-                result.map(::categoryMapper)
-            }
+            it.select("SELECT category_id, code, name FROM Categories WHERE category_id=$1", id)
+                    .mapRow(::categoryMapper)
 
     private fun selectPath(it: Handle, leafId: Long): Flux<Category> =
             it.select("""SELECT category_id, code, name 
                            |FROM Categories c JOIN Categories_Tree_path t ON c.category_id=t.descendant_id
                            |WHERE t.descendant_id = $1
-                           |ORDER BY t.depth DESC""".trimMargin(), leafId).mapResult { result ->
-                result.map(::categoryMapper)
-            }
+                           |ORDER BY t.depth DESC""".trimMargin(), leafId).mapRow(::categoryMapper)
 
     private fun selectCategoryWithNoParent(it: Handle): Flux<Category> =
             it.select("""SELECT category_id, code, name
                            |FROM Categories c JOIN Categories_Tree_Path t ON c.category_id=t.descendant_id
                            |WHERE t.depth = $1 AND t.ancestor_id NOT IN
-                             |(SELECT descendant_id FROM Categories_Tree_Path WHERE depth=$2)""".trimMargin(), 0, 1).mapResult { result ->
-                result.map(::categoryMapper)
-            }
+                             |(SELECT descendant_id FROM Categories_Tree_Path WHERE depth=$2)""".trimMargin(), 0, 1).mapRow(::categoryMapper)
 
     private fun selectCategoryForParent(it: Handle, parentId: Long): Flux<Category> =
             it.select("""SELECT category_id, code, name
                            |FROM Categories c JOIN Categories_Tree_Path t ON c.category_id=t.descendant_id
-                           |WHERE t.ancestor_id = $1 AND t.depth = $2""".trimMargin(), parentId, 1).mapResult { result ->
-                result.map(::categoryMapper)
-            }
+                           |WHERE t.ancestor_id = $1 AND t.depth = $2""".trimMargin(), parentId, 1).mapRow(::categoryMapper)
 }
