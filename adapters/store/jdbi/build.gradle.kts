cc4c1cb77317e1d044f4f02608044b1dc5b1c439
Maciej Malecki <malecki.maciej@gmail.com>
2020-11-23 12:55:43 +0100

    +jdbi

--- adapters/store/jdbi/build.gradle.kts
+++ adapters/store/jdbi/build.gradle.kts
@@ -6,9 +6,12 @@ val collectionsImmutableVersion: String by project
 val jdbiVersion: String by project
 
 dependencies {
+    implementation(project(":domain:inventory"))
+    implementation(project(":domain:production"))
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
     implementation("org.jetbrains.kotlinx:kotlinx-collections-immutable:$collectionsImmutableVersion")
-    implementation(platform("org.jdbi:jdbi-bom:$jdbiVersion"))
+    implementation(platform("org.jdbi:jdbi3-bom:$jdbiVersion"))
+    implementation("org.jdbi:jdbi3-core")
     implementation("org.jdbi:jdbi3-kotlin")
 }
