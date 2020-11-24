7b3bca139d6449a154bc073122e2cab98ca3ac82
Maciej Malecki <malecki.maciej@gmail.com>
2020-11-24 10:59:36 +0100

    units

--- adapters/store/sql/src/main/db/insert_db.sql
+++ adapters/store/sql/src/main/db/insert_db.sql
@@ -1,3 +1,13 @@
+-- Units
 INSERT INTO Units (code, name) VALUES ('s', 'second');
+INSERT INTO Units (code, name) VALUES ('m', 'metre');
 INSERT INTO Units (code, name) VALUES ('kg', 'kilogram');
+INSERT INTO Units (code, name) VALUES ('A', 'ampere');
+INSERT INTO Units (code, name) VALUES ('K', 'kelvin');
+INSERT INTO Units (code, name) VALUES ('mol', 'mole');
+INSERT INTO Units (code, name) VALUES ('cd', 'candela');
+
 INSERT INTO Units (code, name) VALUES ('V', 'volt');
+INSERT INTO Units (code, name) VALUES ('Ω', 'ohm');
+INSERT INTO Units (code, name) VALUES ('F', 'farad');
+INSERT INTO Units (code, name) VALUES ('Hz', 'hertz');
