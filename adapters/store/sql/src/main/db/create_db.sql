51e20e05895cf9560a38ff97eb4f171433c9e75a
Maciej Malecki <malecki.maciej@gmail.com>
2020-11-24 10:45:00 +0100

    sql

--- adapters/store/sql/src/main/db/create_db.sql
+++ adapters/store/sql/src/main/db/create_db.sql
@@ -1,21 +1,37 @@
 CREATE TABLE Units
 (
-    unit_id BIGSERIAL PRIMARY KEY,
-    code    VARCHAR(50) UNIQUE NOT NULL,
-    name    VARCHAR(200)       NOT NULL
+    code VARCHAR(20) PRIMARY KEY,
+    name VARCHAR(50) NOT NULL
 );
 
-CREATE TABLE ItemClasses
+CREATE TABLE Attribute_Types
 (
-    item_class_id BIGSERIAL PRIMARY KEY,
-    name          VARCHAR(50)  NOT NULL,
-    description   VARCHAR(200) NOT NULL
+    name   VARCHAR(50) PRIMARY KEY,
+    unit   VARCHAR(20),
+    scalar BOOLEAN NOT NULL,
+    FOREIGN KEY (unit) REFERENCES Units (code)
+);
+
+CREATE TABLE Attribute_Type_Values
+(
+    attribute_name VARCHAR(50)  NOT NULL,
+    value          VARCHAR(200) NOT NULL,
+    PRIMARY KEY (attribute_name, value),
+    FOREIGN KEY (attribute_name) REFERENCES Attribute_Types (name)
+);
+
+CREATE TABLE Item_Classes
+(
+    name        VARCHAR(50) PRIMARY KEY,
+    description VARCHAR(200) NOT NULL
 );
 
 CREATE TABLE Attributes
 (
-    attribute_id BIGSERIAL PRIMARY KEY,
-    name         VARCHAR(50) NOT NULL
+    name            VARCHAR(50)  NOT NULL,
+    item_class_name VARCHAR(200) NOT NULL,
+    PRIMARY KEY (name, item_class_name),
+    FOREIGN KEY (item_class_name) REFERENCES Item_Classes (name)
 );
 
 CREATE TABLE Categories
--- adapters/store/sql/src/main/db/drop_db.sql
+++ adapters/store/sql/src/main/db/drop_db.sql
@@ -1,3 +1,7 @@
 DROP TABLE Categories_Tree_Path;
 DROP TABLE Categories;
+DROP TABLE Attributes;
+DROP TABLE Item_Classes;
+DROP TABLE Attribute_Type_Values;
+DROP TABLE Attribute_Types;
 DROP TABLE Units;
