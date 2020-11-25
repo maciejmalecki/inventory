18891ce90c40af6cc9e42184a2b947db5102140c
Maciej Malecki <malecki.maciej@gmail.com>
2020-11-25 13:53:28 +0100

    fix in syntax

--- adapters/store/jdbi/src/main/kotlin/mm/inventory/adapters/store/jdbi/items/ItemClassJdbiRepository.kt
+++ adapters/store/jdbi/src/main/kotlin/mm/inventory/adapters/store/jdbi/items/ItemClassJdbiRepository.kt
@@ -9,30 +9,34 @@ import org.jdbi.v3.core.kotlin.withHandleUnchecked
 
 class ItemClassJdbiRepository(private val db: Jdbi) : ItemClassRepository {
     override suspend fun findByName(name: String): ItemClass =
-            db.withHandleUnchecked {
-                val itemClass = it.createQuery("SELECT name, description FROM Item_Classes WHERE name=:name")
+            db.withHandleUnchecked { handle ->
+                // load bare item class
+                val itemClassRec = handle.createQuery("SELECT name, description, unit FROM Item_Classes WHERE name=:name")
                         .bind("name", name)
                         .mapTo<ItemClassRec>()
                         .findOne().get()
 
-                val unit = it.createQuery("SELECT code, name FROM Units WHERE name=:name")
-                        .bind("name", itemClass.unit)
+                // load unit with subsequent SQL (could be also done with JOIN above)
+                val unitRec = handle.createQuery("SELECT code, name FROM Units WHERE code=:code")
+                        .bind("code", itemClassRec.unit)
                         .mapTo<UnitRec>()
                         .findOne().get()
 
-                val attributes = it.createQuery("""SELECT a.name, a.attribute_type, at.scalar, u.code as unit_code, u.name as unit_name
+                // load all attributes for given item class
+                val attributeRecList = handle.createQuery("""SELECT a.name, a.attribute_type, at.scalar, u.code as unit_code, u.name as unit_name
                     |FROM Attributes a JOIN Attribute_Types at ON a.attribute_type = at.name
                     |JOIN Units u ON at.unit = u.code
                     |WHERE item_class_name=:itemClassName""".trimMargin())
-                        .bind("itemClassName", itemClass.name)
+                        .bind("itemClassName", itemClassRec.name)
                         .mapTo<AttributeWithTypeRec>()
                         .list()
 
+                // build up the aggregate out of fetched data
                 ItemClass(
-                        itemClass.name,
-                        itemClass.description,
-                        UnitOfMeasurement(unit.code, unit.name),
-                        attributes.map { attributeWithType ->
+                        itemClassRec.name,
+                        itemClassRec.description,
+                        UnitOfMeasurement(unitRec.code, unitRec.name),
+                        attributeRecList.map { attributeWithType ->
                             when (attributeWithType.scalar) {
                                 true -> Attribute(attributeWithType.name,
                                         ScalarType(
