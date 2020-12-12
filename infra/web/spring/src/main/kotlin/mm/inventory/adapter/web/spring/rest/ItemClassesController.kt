7faadfb185e1df17eb08f9abb852473e7ac9e0a3
Maciej Malecki <malecki.maciej@gmail.com>
2020-12-12 23:47:38 +0100

    /itemClasses endpoint

--- infra/web/spring/src/main/kotlin/mm/inventory/adapter/web/spring/rest/ItemClassesController.kt
+++ infra/web/spring/src/main/kotlin/mm/inventory/adapter/web/spring/rest/ItemClassesController.kt
@@ -1,6 +1,7 @@
 package mm.inventory.adapter.web.spring.rest
 
 import mm.inventory.app.itemsfacade.itemclass.ItemClassFacade
+import mm.inventory.app.itemsfacade.itemclass.ItemClassHeader
 import mm.inventory.domain.items.ItemClass
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
@@ -9,6 +10,9 @@ import org.springframework.web.bind.annotation.RestController
 
 @RestController
 class ItemClassesController(private val itemClassFacade: ItemClassFacade) {
+
+    @GetMapping("/itemClasses")
+    fun itemClasses(): ResponseEntity<List<ItemClassHeader>> = ResponseEntity.ok(itemClassFacade.findAll())
 
     @GetMapping("/itemClasses/{itemClassName}")
     fun itemClass(@PathVariable itemClassName: String): ResponseEntity<ItemClass> {
