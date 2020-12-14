3f5942d7999ed170f039d9073c817a44a76eec40
Maciej Malecki <malecki.maciej@gmail.com>
2020-12-14 11:54:45 +0100

    further styling

--- infra/client/angular/src/app/mat.module.ts
+++ infra/client/angular/src/app/mat.module.ts
@@ -1,12 +1,27 @@
 import {NgModule} from '@angular/core';
 import {MatButtonModule} from '@angular/material/button';
+import {MatGridListModule} from '@angular/material/grid-list';
+import {MatCardModule} from '@angular/material/card';
+import {MatInputModule} from '@angular/material/input';
+import {MatIconModule} from '@angular/material/icon';
+import {MatFormFieldModule} from '@angular/material/form-field';
 
 @NgModule({
   imports: [
-    MatButtonModule
+    MatButtonModule,
+    MatInputModule,
+    MatGridListModule,
+    MatCardModule,
+    MatIconModule,
+    MatFormFieldModule
   ],
   exports: [
-    MatButtonModule
+    MatButtonModule,
+    MatInputModule,
+    MatGridListModule,
+    MatCardModule,
+    MatIconModule,
+    MatFormFieldModule
   ]
 })
 export class MatModule {
