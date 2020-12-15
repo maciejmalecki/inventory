54d7dbe3126c36f8dbf38ee3d518737d69523dae
Maciej Malecki <malecki.maciej@gmail.com>
2020-12-15 16:09:06 +0100

    Fix in form group

--- infra/client/angular/src/app/items/item-classes/item-classes.component.ts
+++ infra/client/angular/src/app/items/item-classes/item-classes.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {Observable} from 'rxjs';
 import {ItemClassHeader, ItemClassService} from '../../shared/services/item-class.service';
+import {FormControl, FormGroup} from '@angular/forms';
 
 @Component({
   selector: 'app-item-classes',
@@ -11,12 +12,17 @@ export class ItemClassesComponent implements OnInit {
 
   itemClasses$: Observable<Array<ItemClassHeader>>;
 
+  formGroup: FormGroup;
+
   constructor(
     private readonly itemClassService: ItemClassService) {
   }
 
   ngOnInit(): void {
     this.itemClasses$ = this.itemClassService.getAllItemClasses();
+    this.formGroup = new FormGroup({
+      search: new FormControl('')
+    });
   }
 
 }
