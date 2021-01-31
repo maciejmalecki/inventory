e8a8cfa34d19c3dc7e81b11362e6bf592d1bd75e
Maciej Malecki <malecki.maciej@gmail.com>
2021-01-31 13:28:47 +0100

    inventory stock

--- domain/inventory/src/main/kotlin/mm/inventory/domain/inventory/ItemStockSelector.kt
+++
@@ -1,9 +0,0 @@
-package mm.inventory.domain.inventory
-
-
-interface ItemStockSelector {
-    /**
-     * Finds stock data for given item.
-     */
-    fun findByItem(itemCode: String): ItemStock
-}
