08add8ded501e997414df89421cdf53dbf8dcaa4
Maciej Malecki <malecki.maciej@gmail.com>
2021-02-01 09:54:57 +0100

    app for stock

--- app/productplanner/build.gradle.kts
+++ app/productplanner/build.gradle.kts
@@ -7,6 +7,7 @@ val collectionsImmutableVersion: String by project
 dependencies {
     implementation(project(":domain:shared"))
     implementation(project(":domain:items"))
+    implementation(project(":domain:inventory"))
     implementation(project(":domain:production"))
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
--- app/productplanner/src/main/kotlin/mm/inventory/app/productplanner/Roles.kt
+++ app/productplanner/src/main/kotlin/mm/inventory/app/productplanner/Roles.kt
@@ -6,3 +6,5 @@ val ITEMS_ROLE = object : Role("items") {}
 val ITEMS_WRITER_ROLE = object : Role("items/writer") {}
 val ITEM_CLASSES_ROLE = object : Role("itemClasses") {}
 val ITEM_CLASSES_WRITER_ROLE = object : Role("itemClasses/writer") {}
+val STOCK_ROLE = object : Role("stock") {}
+val STOCK_WRITER_ROLE = object : Role("stock/writer") {}
